f614ffcaebb914c220dce9cb2d1622e8172225e2
HyeonJin Jung <73989184+HyunJunSik@users.noreply.github.com>
2022-10-23 15:59:00 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5324 ms, Memory: 251444 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,31 @@
+v, e=  map(int, input().split())
+arr = []
+for i in range(e):
+    a, b, cost = map(int, input().split())
+    arr.append((cost, a, b))
+arr.sort()
+
+parent = list(range(v+1))
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x] 
+
+def merge(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+total = 0
+max_cost = 0
+for i in arr:
+    cost, a, b = i
+    if find_parent(parent, a) != find_parent(parent, b):
+        merge(parent, a, b)
+        total += cost
+        max_cost = cost
+print(total- max_cost)
