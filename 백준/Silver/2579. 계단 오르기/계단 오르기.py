37d99fbb5a94fdbb247df2660e51d136ba8db557
HyeonJin Jung <73989184+HyunJunSik@users.noreply.github.com>
2022-11-23 21:34:50 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,12 +1,13 @@
 def Ex2579():
-    # 계단오르기
     n = int(input())
-    arr = [int(input()) for i in range(n)]
+    arr = []
     dp = [0] * (n + 1)
+    for i in range(n):
+        arr.append(int(input()))
     dp[1] = arr[0]
     if n > 1:
-        dp[2] = arr[0] + arr[1]
+        dp[2] = arr[1] + arr[0]
     for i in range(3, n + 1):
-        dp[i] = max(dp[i - 2] + arr[i - 1], dp[i - 3] + arr[i - 2] + arr[i - 1])
-    print(dp[n])
+        dp[i] = max(dp[i - 2], arr[i - 2] + dp[i - 3]) + arr[i - 1]
+    print(dp[-1])
 Ex2579()
