5a0ae1e4ee8d4ab1d819a67387492cf035c93c39
HyeonJin Jung <73989184+HyunJunSik@users.noreply.github.com>
2023-05-27 21:57:08 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 34208 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -2,9 +2,8 @@ from collections import deque
 arr = []
 n = int(input())
 for i in range(n):
-    arr.append(input())
+    arr.append(list(input()))
 visited = [[0] * n for i in range(n)]
-
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
@@ -15,44 +14,29 @@ def bfs(c, row, col):
     visited[row][col] = 1
     while q:
         (y, x) = q.popleft()
-        for dir in range(4):
-            if y + dy[dir] < n and y + dy[dir] >= 0 and x + dx[dir] < n and x + dx[dir] >= 0:
-                if visited[y + dy[dir]][x + dx[dir]] == 0 and arr[y + dy[dir]][x + dx[dir]] == c:
-                    visited[y + dy[dir]][x + dx[dir]] = 1
-                    q.append((y + dy[dir], x + dx[dir]))
+        for i in range(4):
+            if y + dy[i] < n and y + dy[i] >= 0 and x + dx[i] < n and x + dx[i] >= 0:
+                if visited[y + dy[i]][x + dx[i]] == 0 and arr[y + dy[i]][x + dx[i]] == c:
+                    visited[y + dy[i]][x + dx[i]] = 1
+                    q.append((y + dy[i], x + dx[i]))
 
 color = ['R', 'G', 'B']
-
-res_1 = 0
+res = 0
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
             bfs(arr[i][j], i, j)
-            res_1 += 1
-print(res_1, end=' ')
-
+            res += 1
+print(res, end=' ')
 visited = [[0] * n for i in range(n)]
-
-def bfs_1(row, col):
-    global visited
-    global arr
-    q = deque([(row, col)])
-    visited[row][col] = 1
-    while q:
-        (y, x) = q.popleft()
-        for dir in range(4):
-            if y + dy[dir] < n and y + dy[dir] >= 0 and x + dx[dir] < n and x + dx[dir] >= 0:
-                if visited[y + dy[dir]][x + dx[dir]] == 0:
-                    if arr[y + dy[dir]][x + dx[dir]] == 'R' or arr[y + dy[dir]][x + dx[dir]] == 'G':
-                        visited[y + dy[dir]][x + dx[dir]] = 1
-                        q.append((y + dy[dir], x + dx[dir]))
-res_1 = 0
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 'G':
+            arr[i][j] = 'R'
+res = 0
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
-            if arr[i][j] == 'B':
-                bfs(arr[i][j], i, j)
-            else:
-                bfs_1(i, j)
-            res_1 += 1
-print(res_1)
+            bfs(arr[i][j], i, j)
+            res += 1
+print(res)
