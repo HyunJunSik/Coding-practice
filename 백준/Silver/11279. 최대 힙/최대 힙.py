d5a30f2eefecc38cc4cd4ba9e817660b7c5214cd
HyeonJin Jung <73989184+HyunJunSik@users.noreply.github.com>
2023-05-29 02:26:59 +0900

    [Silver II] Title: 최대 힙, Time: 232 ms, Memory: 120820 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+from heapq import heappush, heappop
+import sys
+
+arr = []
+for i in range(int(sys.stdin.readline())):
+    c = int(sys.stdin.readline())
+    if c == 0:
+        if len(arr) == 0:
+            print(0)
+        else:
+            print(heappop(arr)[1])
+    else:
+        heappush(arr, (-c, c))
